da5a20f2dd036bec030cc99db724163d6cbf7bc0
Stewart Dulaney <sdulaney@users.noreply.github.com>
2018-07-25 16:16:54 -0700

    add link to register button

--- public/js/app.js
+++ public/js/app.js
@@ -9,3 +9,6 @@ $('#btn_login').on('click', function() {
 $('#btn_new_transaction').on('click', function() {
   window.location.href = "http://www.smccs85.com/~sdulaney/project/insert_transaction_form.php";
 });
+$('#btn_register').on('click', function() {
+  window.location.href = "http://www.smccs85.com/~sdulaney/project/register_form.php";
+});
